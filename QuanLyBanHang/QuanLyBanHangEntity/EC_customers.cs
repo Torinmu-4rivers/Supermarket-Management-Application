e8102a4551453a0123c1b4edc659ab071c32f72b
ngtrduc94 <ngtrduc94@gmail.com>
2014-12-10 17:55:16 +0700

    Entity1

---
+++ QuanLyBanHang/QuanLyBanHangEntity/EC_customers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanHangEntity
+{
+    public class EC_customers
+    {
+        private string _cust_id;
+        private string _cust_name;
+        private string _cust_gender;
+        private string _cust_dob;
+        private string _cust_phone_number;
+        private string _cust_address;
+    }
+}
