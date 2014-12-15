3a2ce0efa4d125e1768b92257fc7182f12a77e93
LPCH1994 <hsblh32@gmail.com>
2014-12-15 17:44:40 +0700

    git bug @@

---
+++ QuanLyBanHang/QuanLyBanHang/LoginForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace QuanLyBanHang
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbuser = new System.Windows.Forms.Label();
+            this.lbpass = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnexit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnlogin.Location = new System.Drawing.Point(201, 358);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(85, 40);
+            this.btnlogin.TabIndex = 0;
+            this.btnlogin.Text = "Log in";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(250, 318);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lbuser
+            // 
+            this.lbuser.AutoSize = true;
+            this.lbuser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbuser.Location = new System.Drawing.Point(131, 287);
+            this.lbuser.Name = "lbuser";
+            this.lbuser.Size = new System.Drawing.Size(91, 20);
+            this.lbuser.TabIndex = 2;
+            this.lbuser.Text = "Username";
+            // 
+            // lbpass
+            // 
+            this.lbpass.AutoSize = true;
+            this.lbpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbpass.Location = new System.Drawing.Point(131, 318);
+            this.lbpass.Name = "lbpass";
+            this.lbpass.Size = new System.Drawing.Size(86, 20);
+            this.lbpass.TabIndex = 3;
+            this.lbpass.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox2.Location = new System.Drawing.Point(250, 287);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(218, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(112, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(384, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnexit
+            // 
+            this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexit.ForeColor = System.Drawing.Color.Red;
+            this.btnexit.Location = new System.Drawing.Point(326, 358);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(85, 40);
+            this.btnexit.TabIndex = 6;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(606, 422);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbpass);
+            this.Controls.Add(this.lbuser);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnlogin);
+            this.Name = "LoginForm";
+            this.Text = "Welcome";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbuser;
+        private System.Windows.Forms.Label lbpass;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnexit;
+    }
+}
--- QuanLyBanHang/QuanLyBanHangDAL/SQL_employees_job.cs
+++ QuanLyBanHang/QuanLyBanHangDAL/SQL_employees_job.cs
@@ -21,12 +21,12 @@ namespace QuanLyBanHangDAL
             cn.ThucThiCauLenhSQL("UPDATE employee_jobs SET ew_name = '" + et.Ew_name + "', ew_description = '" + et.Ew_description + "' where ew_id = '" + et.Ew_id + "'");
 
         }
-        public void XoaDuLieu (EC_employee_jobs et)
+        public void XoaDuLieu(EC_employee_jobs et)
         {
             cn.ThucThiCauLenhSQL("DELETE from employee_jobs where ew_id='" + et.Ew_id + "'");
 
         }
-        public DataTable getInfoEW (string DieuKien)
+        public DataTable getInfoEW(string DieuKien)
         {
             return cn.GetDataTable("select * from employee_jobs " + DieuKien);
         }
